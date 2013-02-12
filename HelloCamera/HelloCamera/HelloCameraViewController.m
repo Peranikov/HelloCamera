4f7571a76bd5f54f028985746b5dc9c94c8cfd63
Yuto Matsukubo <matsukubo@Yuto-no-Mac-mini.local>
2013-02-12 21:38:11 +0900

    カメラAPIの呼び出し

--- HelloCamera/HelloCamera/HelloCameraViewController.m
+++ HelloCamera/HelloCamera/HelloCameraViewController.m
@@ -18,6 +18,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    if ([UIImagePickerController isSourceTypeAvailable:sourceType]) {
+        picker.delegate = self;
+        picker.sourceType = sourceType;
+        [self presentModalViewController:picker animated:YES];
+    }
 }
 
 - (void)didReceiveMemoryWarning
